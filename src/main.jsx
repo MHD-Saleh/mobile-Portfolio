4d214e2c6b1cb5655f35272fb6db90ec8defadfe
Mohammed Alsaleh <mohammed.saleh.alsaleh.98@gmail.com>
2024-06-01 19:27:15 +0300

    add ga

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import ReactGA from "react-ga4";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactGA.initialize("G-3WTR3PED9S");
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
