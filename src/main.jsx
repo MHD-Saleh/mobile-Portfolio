e6c157ac4319ef1ce8a08d8d26d187786029b970
Mohammed Alsaleh <mohammed.saleh.alsaleh.98@gmail.com>
2024-06-01 19:38:13 +0300

    add vercel analysis

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import ReactGA from "react-ga4";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactGA.initialize("G-3WTR3PED9S");
 ReactGA.send({
   hitType: "pageview",
   page: window.location.pathname,
 });
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Analytics />
     <App />
   </React.StrictMode>
 );
